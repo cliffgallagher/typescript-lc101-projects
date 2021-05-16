487bfc09a6d4a900e2f5cce29505dc1f7e8ce268
Cliff Gallagher <scliffordgallagher@gmail.com>
2021-05-16 16:05:40 -0500

    finished studio

---
+++ studio/Cargo.ts
@@ -0,0 +1,10 @@
+export class Cargo implements Payload {
+    massKg: number;
+    material: string;
+    constructor(massKg: number, material: string) {
+        this.massKg = massKg;
+        this.material = material;
+    }
+}
+
+import { Payload } from './Payload'
